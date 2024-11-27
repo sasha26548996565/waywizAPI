914d05682d989c737930227bf198c725296b4937
sasha26548996565 <68546453+sasha26548996565@users.noreply.github.com>
2024-11-27 19:49:45 +0500

    exceptions bot

--- app/Http/Controllers/Api/Openai/BotController.php
+++ app/Http/Controllers/Api/Openai/BotController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api\Openai;
 
 use App\Models\User;
 use App\Models\Message;
+use Illuminate\Http\Response;
 use Illuminate\Contracts\View\View;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -26,29 +27,36 @@ class BotController extends Controller
 
     public function sendMessage(MessageRequest $request): JsonResponse
     {
-        $params = $request->validated();
-        $user = User::findOrFail(Auth::user()->id);
+        try {
+            $params = $request->validated();
+            $user = User::findOrFail(Auth::user()->id);
 
-        if ($user->isAvailable() == false) {
-            throw new \Exception('No available!');
+            if ($user->isAvailable() == false) {
+                throw new \Exception('No available!');
+            }
+
+            $message = $this->gptBotApi->sendMessage($params['text']);
+
+            Message::create([
+                'text' => $params['text'],
+                'user_id' => $user->id,
+                'is_user' => true
+            ]);
+
+            Message::create([
+                'text' => $message,
+                'user_id' => $user->id,
+                'is_user' => false
+            ]);
+
+            return response()->json([
+                'message' => $message
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status' => false,
+                'message' => $exception->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        $message = $this->gptBotApi->sendMessage($params['text']);
-
-        Message::create([
-            'text' => $params['text'],
-            'user_id' => $user->id,
-            'is_user' => true
-        ]);
-
-        Message::create([
-            'text' => $message,
-            'user_id' => $user->id,
-            'is_user' => false
-        ]);
-
-        return response()->json([
-            'message' => $message
-        ]);
     }
 }
