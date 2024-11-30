f738cd3be406c83b1993f9738fb0caa352055a02
sasha26548996565 <68546453+sasha26548996565@users.noreply.github.com>
2024-11-30 23:35:38 +0500

    add title admin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,26 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/sass/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
-        @else
-            <style>
-            </style>
-        @endif
-    </head>
-    <body>
-        <div id="app">
-            <Index></Index>
-        </div>
-    </body>
-</html>
+@section('content')
+    <Index></Index>
+@endsection
